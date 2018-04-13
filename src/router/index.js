fa8755924e6636dba24d156b3eb943c6648d88ca
fangjialiang258351353 <18175361553@163.com>
2018-04-13 18:44:32 +0800

    0413

--- src/router/index.js
+++ src/router/index.js
@@ -47,7 +47,7 @@ import Login from '@/components/login'
 Vue.use(Router)
 
 let router = new Router({
-  mode: 'history',
+  mode: 'hash',
   linkActiveClass: 'is-active',
   routes: [
     {
@@ -85,10 +85,46 @@ let router = new Router({
           }
         }
       ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
 
+router.beforeEach((to, from, next) => {
+
+  let bl = to.matched.some(function(item){
+    return item.meta.login
+  })
+
+  if(to.matched.some((item) => item.meta.login)){
+    let info = router.app.$local.fetch("miaov")
+
+    if(info.login){  // 已经登录
+        next()
+    }else{
+      router.push({
+        path: '/login',
+        query: {
+          redirect: to.path.slice(1)
+        }
+      })
+    }
+
+  }else{
+    next()
+  }
+
+
+
+})
 
 
 
